1c29639ec724fcfd3b04810a1867604ae2a2a6d2
lookerson <lookerson.wm@gmail.com>
2015-12-25 17:51:06 +0800

    新建CShellCode类

---
+++ ShellCodeFramwork/ShellCode.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "ShellCode.h"
+
+CShellCode::CShellCode()
+{
+}
+
+
+CShellCode::~CShellCode()
+{
+}
---
+++ ShellCodeFramwork/ShellCode.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class CShellCode
+{
+
+public:
+    CShellCode();
+    ~CShellCode();
+};
+
