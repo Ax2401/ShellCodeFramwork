cbc5a796e4925ad905d5c7c07b346b24bbbcc1b1
lookerson <lookerson.wm@gmail.com>
2015-12-26 17:08:46 +0800

    创建了一个CMySampleShellCode类
    这个类主要是用来演示如何编写一个ShellCode

---
+++ ShellCodeFramwork/MySampleShellCode.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "MySampleShellCode.h"
+
+
+CMySampleShellCode::CMySampleShellCode()
+{
+}
+
+
+CMySampleShellCode::~CMySampleShellCode()
+{
+}
+
+void CMySampleShellCode::Run()
+{
+}
---
+++ ShellCodeFramwork/MySampleShellCode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "ShellCode.h"
+
+class CMySampleShellCode :
+    public CShellCode
+{
+public:
+    CMySampleShellCode();
+    ~CMySampleShellCode();
+
+    void Run();
+};
+
